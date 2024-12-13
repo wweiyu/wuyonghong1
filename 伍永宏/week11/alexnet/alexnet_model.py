f95a7792b9b17011d9b29e278fa5d2020a29619d
wweiyu <whonghome@163.com>
2024-12-13 23:25:58 +0800

    Create alexnet_model.py

---
+++ 伍永宏/week11/alexnet/alexnet_model.py
@@ -0,0 +1,54 @@
+from keras.models import  Sequential
+from keras.layers import Conv2D,MaxPooling2D,Flatten,BatchNormalization,Dense,Dropout
+
+def alexnet_model(input_shape = (224,224,3),output_shape = 2):
+    model = Sequential()
+
+    model.add(Conv2D(filters=48,
+                     kernel_size=(11,11),
+                     strides = (4,4),
+                     padding='valid',
+                     input_shape= input_shape,
+                     activation='relu'))
+
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((3,3),strides=(2,2),padding='valid'))
+
+    model.add(Conv2D(filters=128,
+                     kernel_size=(5,5),
+                     strides = (1,1),
+                     padding='same',
+                     activation='relu'))
+    model.add(BatchNormalization())
+
+    model.add(MaxPooling2D((3,3),strides=(2,2),padding='valid'))
+
+    model.add(Conv2D(filters=192,
+                     kernel_size=(3,3),
+                     strides = (1,1),
+                     padding='same',
+                     activation='relu'))
+
+    model.add(Conv2D(filters=192,
+                     kernel_size=(3,3),
+                     strides = (1,1),
+                     padding='same',
+                     activation='relu'))
+
+    model.add(Conv2D(filters=128,
+                     kernel_size=(3,3),
+                     strides = (1,1),
+                     padding='same',
+                     activation='relu'))
+    model.add(MaxPooling2D((3,3),strides=(2,2),padding='valid'))
+
+    model.add(Flatten())
+    model.add(Dense(1024,activation = 'relu'))
+    model.add(Dropout(0.25))
+
+    model.add(Dense(1024,activation = 'relu'))
+    model.add(Dropout(0.25))
+
+    model.add(Dense(output_shape,activation = 'softmax'))
+    return model
+
