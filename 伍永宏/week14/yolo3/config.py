332717577f475edf01ceb11a9659f9754130b003
wweiyu <whonghome@163.com>
2025-01-03 23:26:00 +0800

    Create config.py

---
+++ 伍永宏/week14/yolo3/config.py
@@ -0,0 +1,12 @@
+norm_decay = 0.99
+norm_epsilon = 1e-3
+pre_train = True
+num_anchors = 9
+num_classes = 80
+
+obj_threshold = 0.5
+nms_threshold = 0.5
+
+anchors_path = 'model_data/yolo_anchors.txt'
+classes_path = 'model_data/coco_classes.txt'
+weight_path = 'model_data/yolov3.weights'
