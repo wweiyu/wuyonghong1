4f048ae0af0b84a3f50d892f98ec2d65775ddd92
wweiyu <whonghome@163.com>
2025-01-17 23:23:18 +0800

    Create utils.py

---
+++ 伍永宏/week16/yolov5/utils.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def get_classes(classes_path):
+    with open(classes_path, encoding='utf-8') as f:
+        class_names = f.readlines()
+    class_names = [c.strip() for c in class_names]
+    return class_names, len(class_names)
+
+def get_anchors(anchors_path):
+    with open(anchors_path, encoding='utf-8') as f:
+        anchors = f.readline()
+    anchors = [float(x) for x in anchors.split(',')]
+    anchors = np.array(anchors).reshape(-1, 2)
+    return anchors, len(anchors)
