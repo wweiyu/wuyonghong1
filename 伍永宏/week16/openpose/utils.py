a0171000de4c936be1555fb82e1c7cd9b61da61f
wweiyu <whonghome@163.com>
2025-01-17 23:31:47 +0800

    Create utils.py

---
+++ 伍永宏/week16/openpose/utils.py
@@ -0,0 +1,55 @@
+import numpy as np
+import math
+import cv2
+
+def transfer(model, model_weights):
+    fixed = {}
+    for name in model.state_dict().keys():
+        fixed[name] = model_weights['.'.join(name.split('.')[1:])]
+    model.load_state_dict(fixed)
+
+
+def padRightDownCorner(image,stride,padvalue):
+    h,w = image.shape[:2]
+    # up left,down right
+    pad = [0,0,0,0]
+    pad[2] = 0 if w%stride == 0 else stride - w%stride
+    pad[3] = 0 if h%stride == 0 else stride - h%stride
+
+    image_pad = np.pad(image,((pad[0],pad[3]),(pad[1],pad[2]),(0,0)),mode='constant',constant_values = (padvalue,padvalue))
+    return image_pad,pad
+
+def draw_bodypose(canvas, candidate, subset):
+    stickwidth = 4
+    limbSeq = [[2, 3], [2, 6], [3, 4], [4, 5], [6, 7], [7, 8], [2, 9], [9, 10], \
+               [10, 11], [2, 12], [12, 13], [13, 14], [2, 1], [1, 15], [15, 17], \
+               [1, 16], [16, 18], [3, 17], [6, 18]]
+
+    colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0], [85, 255, 0], [0, 255, 0], \
+              [0, 255, 85], [0, 255, 170], [0, 255, 255], [0, 170, 255], [0, 85, 255], [0, 0, 255], [85, 0, 255], \
+              [170, 0, 255], [255, 0, 255], [255, 0, 170], [255, 0, 85]]
+    for i in range(18):
+        for n in range(len(subset)):
+            index = int(subset[n][i])
+            if index == -1:
+                continue
+            x, y = candidate[index][0:2]
+            cv2.circle(canvas, (int(x), int(y)), 4, colors[i], thickness=-1)
+    for i in range(17):
+        for n in range(len(subset)):
+            index = subset[n][np.array(limbSeq[i]) - 1]
+            if -1 in index:
+                continue
+            cur_canvas = canvas.copy()
+            Y = candidate[index.astype(int), 0]
+            X = candidate[index.astype(int), 1]
+            mX = np.mean(X)
+            mY = np.mean(Y)
+            length = ((X[0] - X[1]) ** 2 + (Y[0] - Y[1]) ** 2) ** 0.5
+            angle = math.degrees(math.atan2(X[0] - X[1], Y[0] - Y[1]))
+            polygon = cv2.ellipse2Poly((int(mY), int(mX)), (int(length / 2), stickwidth), int(angle), 0, 360, 1)
+            cv2.fillConvexPoly(cur_canvas, polygon, colors[i])
+            canvas = cv2.addWeighted(canvas, 0.4, cur_canvas, 0.6, 0)
+    return canvas
+
+
